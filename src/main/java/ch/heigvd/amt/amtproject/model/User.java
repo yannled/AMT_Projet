c27bc94704888e58026cf6d78fa3f3e083e72042
zutt <yannlederrey@gmail.com>
2018-10-05 10:27:43 +0200

    template du servlet, model User

---
+++ src/main/java/ch/heigvd/amt/amtproject/model/User.java
@@ -0,0 +1,60 @@
+package ch.heigvd.amt.amtproject.model;
+
+import javax.naming.Name;
+
+public class User {
+
+    private String name;
+    private String lastName;
+    private String password;
+    private String email;
+    private boolean admin;
+
+    public User(String name, String lastName, String password, String email, boolean admin) {
+        this.name = name;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+}
---
+++ src/main/java/ch/heigvd/amt/amtproject/presentation/MainServlet.java
@@ -0,0 +1,76 @@
+package ch.heigvd.amt.amtproject.presentation;
+
+import ch.heigvd.amt.amtproject.model.User;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+
+
+@WebServlet("/")
+public class MainServlet extends javax.servlet.http.HttpServlet {
+
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        //request.getRequestDispatcher("/WEB-INF/pages/view.jsp?page="+page.getCurrentPage()).forward(request, response);
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String lastname = request.getParameter("lastname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String SecondPassword = request.getParameter("secondPassword");
+        User user = new User(name,lastname,password,email,true);
+
+        boolean login = true;
+
+        /*
+        if (!email.contains("@")) {
+            errorEmail.setErrorText( "bad email structure !");
+            errorEmail.setError(true);
+            login = false;
+        }
+        else {
+            errorEmail.setValue(email);
+        }
+
+        if (name.equals("")){
+            errorName.setErrorText("name empty !");
+            errorName.setError(true);
+            login = false;
+        }
+        else {
+            errorName.setValue(name);
+        }
+
+        if (lastname.isEmpty()){
+            errorLastName.setErrorText("lastName empty !");
+            errorLastName.setError(true);
+            login = false;
+        }
+        else {
+            errorLastName.setValue(lastname);
+        }
+        */
+
+        if (login) {
+            //User user = new User(name, lastname, email);
+            //users.add(user);
+            doGet(request, response);
+            return;
+        }
+
+
+        //request.setAttribute("value", value);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}

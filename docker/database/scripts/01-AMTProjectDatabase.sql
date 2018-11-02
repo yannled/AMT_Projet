36926f25ff9d1eb0e96dac9eef35c2ba994a43de
mitraillet <steve.henriquet@gmail.com>
2018-11-02 09:56:26 +0100

    Fix issue with fill database

--- docker/database/scripts/01-AMTProjectDatabase.sql
+++ docker/database/scripts/01-AMTProjectDatabase.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE AMTProjectDatabase;
+CREATE DATABASE IF NOT EXISTS AMTProjectDatabase;
 
 USE AMTProjectDatabase;
 
-CREATE TABLE tbStatus (
+CREATE TABLE IF NOT EXISTS tbStatus (
 	statusId INT UNSIGNED PRIMARY KEY,
 	status varchar(25) NOT NULL UNIQUE
 );
 
-CREATE TABLE tbPrivilege (
+CREATE TABLE IF NOT EXISTS tbPrivilege (
 	privilegeId INT UNSIGNED PRIMARY KEY,
 	privilege varchar(25) NOT NULL UNIQUE
 );
 
-CREATE TABLE tbUser (
+CREATE TABLE IF NOT EXISTS tbUser (
 	userId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	usrLastName varchar(255) NOT NULL,
+	userLastName varchar(255) NOT NULL,
     userFirstName varchar(255) NOT NULL,
     userEmail varchar(255) NOT NULL UNIQUE,
 	userSel char(32) NOT NULL,
@@ -26,8 +26,7 @@ CREATE TABLE tbUser (
 	CONSTRAINT fk_tbUser_tbPrivilege FOREIGN KEY (privilegeId) REFERENCES tbPrivilege (privilegeId) ON UPDATE CASCADE
 );
 
-
-CREATE TABLE tbProject (
+CREATE TABLE IF NOT EXISTS tbProject (
 	projectId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	projectName varchar(255) NOT NULL,
     projectDescrition TEXT,
@@ -36,7 +35,7 @@ CREATE TABLE tbProject (
 	APISecret varchar(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE tbUserProject (
+CREATE TABLE IF NOT EXISTS tbUserProject (
 	userId INT UNSIGNED NOT NULL,
 	projectId INT UNSIGNED NOT NULL,
 	CONSTRAINT pk_user_project PRIMARY KEY (userId, projectId),

164cb70e16ace1d70a6ed29d2cccc62ab141255b
mitraillet <steve.henriquet@gmail.com>
2018-11-14 11:34:43 +0100

    Keep correct pagination when modifying app

--- src/main/java/ch/heigvd/amt/amtproject/presentation/ApplicationsServlet.java
+++ src/main/java/ch/heigvd/amt/amtproject/presentation/ApplicationsServlet.java
@@ -1,8 +1,6 @@
 package ch.heigvd.amt.amtproject.presentation;
 
-import ch.heigvd.amt.amtproject.business.DAO.ApplicationDAO;
 import ch.heigvd.amt.amtproject.business.DAO.ApplicationDAOLocal;
-import ch.heigvd.amt.amtproject.business.DAO.UserDAO;
 import ch.heigvd.amt.amtproject.model.Application;
 import ch.heigvd.amt.amtproject.model.User;
 import ch.heigvd.amt.amtproject.model.Pagination;
@@ -12,10 +10,10 @@ import javax.ejb.EJB;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.Console;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -38,22 +36,20 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
         new VerifySession(request.getSession(), request, response).redirectIfNoUser();
 
         HttpSession session = request.getSession();
-        User currentUser = (User) session.getAttribute("user");
+        User currentUser = (User)session.getAttribute("user");
 
         // Test if the request come from the admin View, if yes there is some query string defined and that's mean we
         // we want to see the application of an other user.
         // else we take our application
-        try {
-            if (request.getParameterMap().containsKey("action") && request.getParameterMap().containsKey("userEmail") && request.getParameter("action").equals("SHOWAPPUSER") && currentUser.isAdmin()) {
-                applications = applicationDAO.getProjectsByUser(request.getParameter("userEmail"));
-            } else {
-                applications = applicationDAO.getProjectsByUser(currentUser.getEmail());
-            }
-        } catch (Exception e) {
-            response.getWriter().println("There was a problem when we get the Applications from a this user in the database \n " + e);
+        if(request.getParameterMap().containsKey("showUser") && request.getParameterMap().containsKey("userEmail") && request.getParameter("showUser").equals("SHOWUSER") && currentUser.isAdmin()){
+
+            applications = applicationDAO.getProjectsByUser(request.getParameter("userEmail"));
+        }
+        else{
+          applications = applicationDAO.getProjectsByUser(currentUser.getEmail());
         }
 
-        pagination = new Pagination(1, 1);
+        pagination = new Pagination(1,1);
 
         //PAGINATION
         int recordPerPage = 2;
@@ -62,7 +58,7 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
         pagination.setRecordsPerPage(recordPerPage, applications.size());
 
         // define if a page is choose
-        if (request.getParameter("value") != null)
+        if(request.getParameter("value") != null)
             pagination.setCurrentPage(Integer.parseInt(request.getParameter("value")));
 
         // define position of first Element and last element
@@ -70,10 +66,10 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
         int lastElement = pagination.getLastElement(applications.size());
 
         // define a sublist with element to show
-        List<Application> tempList = applications.subList(firstElement, lastElement);
+        List<Application> tempList = applications.subList(firstElement,lastElement);
 
         int noOfRecords = applications.size();
-        int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / pagination.getRecordsPerPage());
+        int noOfPages = (int)Math.ceil(noOfRecords * 1.0 / pagination.getRecordsPerPage());
 
         request.setAttribute("isAdmin", currentUser.isAdmin());
         request.setAttribute("applications", tempList);
@@ -81,7 +77,8 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
         request.setAttribute("currentPage", pagination.getCurrentPage());
 
         // We give the attributes comming from the admin part (there will be tested if empty in the jsp file
-        request.setAttribute("action", request.getParameter("action"));
+
+        request.setAttribute("showUser", request.getParameter("showUser"));
         request.setAttribute("userEmail", request.getParameter("userEmail"));
 
         request.getRequestDispatcher(VUE).forward(request, response);
@@ -91,7 +88,7 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
         new VerifySession(request.getSession(), request, response).redirectIfNoUser();
 
         HttpSession session = request.getSession();
-        User currentUser = (User) session.getAttribute("user");
+        User currentUser = (User)session.getAttribute("user");
         long currentUserId = currentUser.getId();
 
         String action = request.getParameter("action");
@@ -151,5 +148,6 @@ public class ApplicationsServlet extends javax.servlet.http.HttpServlet {
             }
             doGet(request, response);
         }
+        doGet(request, response);
     }
 }
